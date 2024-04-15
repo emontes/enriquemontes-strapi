f14691e50b906535e605154846995e3cbcde325d
Enrique Montes <emontes@dstr.net>
2024-04-15 18:12:47 -0500

    bugfix display cloudinary images in admin panel

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,30 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	"strapi::errors",
+	// "strapi::security",
+	{
+		name: "strapi::security",
+		config: {
+			contentSecurityPolicy: {
+				directives: {
+					"img-src": [
+						"'self'",
+						"data:",
+						"blob:",
+						"https://res.cloudinary.com",
+						"https://market-assets.strapi.io",
+						// Aquí puedes poner cualquier otra fuente que necesites
+					],
+					// Puedes necesitar agregar otras directivas aquí
+				},
+			},
+		},
+	},
+	"strapi::cors",
+	"strapi::poweredBy",
+	"strapi::logger",
+	"strapi::query",
+	"strapi::body",
+	"strapi::session",
+	"strapi::favicon",
+	"strapi::public",
 ];
